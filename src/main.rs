35d739227da8cae2b23d886384f7b04a53d2b8ff
morifuji <marooon88@gmail.com>
2024-06-15 15:26:46 +0900

    add: 2.4

--- src/main.rs
+++ src/main.rs
@@ -1,17 +1,29 @@
 fn main() {
-    let mut stack = vec![];
 
-    stack.push(42);
-    stack.push(36);
+    let mut stack: Vec<i32> = vec!();
 
+    for line in std::io::stdin().lines() {
+        if let Ok(_line) = line {
+            let words = _line.split(" ").collect::<Vec<&str>>();
+            // println!("Line: {words:?}. line: {_line}");
 
-    add(&mut stack);
+            for word in words {
+                if let Ok(word) = word.parse::<i32>() {
+                    stack.push(word)
+                } else {
+                    match word {
+                        "+" => add(&mut stack),
+                        "-" => sub(&mut stack),
+                        "*" => mul(&mut stack),
+                        "/" => div(&mut stack),
+                        _ => panic!("{word} is aaaa")
+                    }
+                }
+            }
+        }
 
-    stack.push(22);
-
-    add(&mut stack);
-    
-    println!("stack: {stack:?}");
+        println!("Stack is {stack:?}");
+    }
 }
 
 
@@ -19,4 +31,22 @@ fn add(stack: &mut Vec<i32>) {
     let left_hand = stack.pop().unwrap();
     let right_hand = stack.pop().unwrap();
     stack.push(left_hand+right_hand);
+}
+
+fn sub(stack: &mut Vec<i32>) {
+    let right_hand = stack.pop().unwrap();
+    let left_hand = stack.pop().unwrap();
+    stack.push(left_hand-right_hand);
+}
+
+fn mul(stack: &mut Vec<i32>) {
+    let left_hand = stack.pop().unwrap();
+    let right_hand = stack.pop().unwrap();
+    stack.push(right_hand*left_hand);
+}
+
+fn div(stack: &mut Vec<i32>) {
+    let right_hand = stack.pop().unwrap();
+    let left_hand = stack.pop().unwrap();
+    stack.push(left_hand/right_hand);
 }
