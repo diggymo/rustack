849e66ee7c64d26029a9a7290df3b8c72bf05cfe
morifuji <marooon88@gmail.com>
2024-06-15 15:16:19 +0900

    add: 2.2

--- src/main.rs
+++ src/main.rs
@@ -1,3 +1,22 @@
 fn main() {
-    println!("Hello, world!");
+    let mut stack = vec![];
+
+    stack.push(42);
+    stack.push(36);
+
+
+    add(&mut stack);
+
+    stack.push(22);
+
+    add(&mut stack);
+    
+    println!("stack: {stack:?}");
+}
+
+
+fn add(stack: &mut Vec<i32>) {
+    let left_hand = stack.pop().unwrap();
+    let right_hand = stack.pop().unwrap();
+    stack.push(left_hand+right_hand);
 }
