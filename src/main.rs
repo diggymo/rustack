9cf8f4b97e96798d623de831aacf0ffd5fe494e7
morifuji <marooon88@gmail.com>
2024-06-23 13:27:23 +0900

    add: 3.4: 連続した括弧を解析できるように

--- src/main.rs
+++ src/main.rs
@@ -1,9 +1,24 @@
 // fn recognizer(input: &str) -> &str;
 
+use std::{ops::Range, str::Chars};
+
 #[derive(Debug, PartialEq, Eq)]
 enum Token {
     Ident,
     Number,
+    LParen,
+    RParen,
+}
+
+/** 不要な気がする... */
+fn advance_char(input: &str) -> &str {
+    let mut chars = input.chars();
+    chars.next();
+    chars.as_str()
+}
+
+fn peek_char(input: &str) -> Option<char> {
+    input.chars().next()
 }
 
 fn source(mut input: &str) -> Vec<Token> {
@@ -30,6 +45,10 @@ fn token(input: &str) -> (&str, Option<Token>) {
         return (i, Some(number_res));
     }
 
+    if let (i, Some(paren_res)) = paren(trimed_input) {
+        return (i, Some(paren_res));
+    }
+
     (input, None)
 }
 
@@ -44,16 +63,10 @@ fn whitespace(mut input: &str) -> &str {
 }
 
 fn number(mut input: &str) -> (&str, Option<Token>) {
-    let first_char = input.chars().next();
+    let first_char = peek_char(input);
     if !matches!(first_char, Some(_x @ ('-' | '+' | '.' | '0'..='9'))) {
         return (input, None);
     }
-    // let mut result: &str = input;
-    // while matches!(input.chars().next(), Some(_x @ ('.'|'0'..='9'))) {
-    //     let mut chars = input.chars();
-    //     chars.next();
-    //     input = chars.as_str();
-    // }
 
     let mut chars = input.chars();
     while matches!(chars.next(), Some(_x @ ('.' | '0'..='9'))) {
@@ -63,7 +76,7 @@ fn number(mut input: &str) -> (&str, Option<Token>) {
 }
 
 fn ident(mut input: &str) -> (&str, Option<Token>) {
-    if !matches!(input.chars().next(), Some(_x @ ('a'..='z' | 'A'..='Z'))) {
+    if !matches!(peek_char(input), Some(_x @ ('a'..='z' | 'A'..='Z'))) {
         return (input, None);
     }
     let mut chars = input.chars();
@@ -71,6 +84,22 @@ fn ident(mut input: &str) -> (&str, Option<Token>) {
         input = chars.as_str();
     }
     (input, Some(Token::Ident))
+}
+
+fn paren(input: &str) -> (&str, Option<Token>) {
+    if peek_char(input) == Some('(') {
+        let mut chars = input.chars();
+        chars.next();
+        return (chars.as_str(), Some(Token::LParen));
+    }
+
+    if peek_char(input) == Some(')') {
+        let mut chars = input.chars();
+        chars.next();
+        return (chars.as_str(), Some(Token::RParen));
+    }
+
+    (input, None)
 }
 
 #[cfg(test)]
@@ -135,6 +164,22 @@ mod test {
     fn test_source_empty() {
         let result = source("");
         assert_eq!(result, vec![]);
+    }
+
+    #[test]
+    fn test_source_paren() {
+        let result = source("() (())");
+        assert_eq!(
+            result,
+            vec![
+                Token::LParen,
+                Token::RParen,
+                Token::LParen,
+                Token::LParen,
+                Token::RParen,
+                Token::RParen,
+            ]
+        );
     }
 }
 
