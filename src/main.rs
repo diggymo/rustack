240d17f49bb8f92c0b3e932e943c211c9ae4136d
morifuji <marooon88@gmail.com>
2024-06-23 13:06:25 +0900

    add: 3.3: ソースコードのパーサー

--- src/main.rs
+++ src/main.rs
@@ -1,5 +1,41 @@
 // fn recognizer(input: &str) -> &str;
 
+#[derive(Debug, PartialEq, Eq)]
+enum Token {
+    Ident,
+    Number,
+}
+
+fn source(mut input: &str) -> Vec<Token> {
+    let mut result = vec![];
+    loop {
+        if let (new_input, Some(token_type)) = token(input) {
+            result.push(token_type);
+            input = new_input;
+        } else {
+            break;
+        }
+        
+    }
+
+    result
+}
+
+fn token(input: &str) -> (&str, Option<Token>) {
+    let trimed_input = whitespace(input);
+    if let (i, Some(ident_res)) = ident(trimed_input) {
+        return (i, Some(ident_res));
+    }
+
+    if let (i, Some(number_res)) = number(trimed_input) {
+        return (i, Some(number_res));
+    }
+
+    (input, None)
+}
+
+
+
 fn whitespace(mut input: &str) -> &str {
     // input.chars()は新しいiteratorを生み出す。
     let mut chars = input.chars();
@@ -10,39 +46,38 @@ fn whitespace(mut input: &str) -> &str {
     input
 }
 
-fn number(mut input: &str) -> &str {
+fn number(mut input: &str) -> (&str, Option<Token>) {
     let first_char = input.chars().next();
-    if matches!(first_char, Some(_x @ ('-'|'+'|'.'|'0'..='9'))) {
-        // let mut result: &str = input;
-        // while matches!(input.chars().next(), Some(_x @ ('.'|'0'..='9'))) {
-        //     let mut chars = input.chars();
-        //     chars.next();
-        //     input = chars.as_str();
-        // }
-
-        let mut chars = input.chars();
-        while matches!(chars.next(), Some(_x @ ('.'|'0'..='9'))) {
-            input = chars.as_str();
-        }
+    if !matches!(first_char, Some(_x @ ('-' | '+' | '.' | '0'..='9'))) {
+        return (input, None);
     }
-    input
+    // let mut result: &str = input;
+    // while matches!(input.chars().next(), Some(_x @ ('.'|'0'..='9'))) {
+    //     let mut chars = input.chars();
+    //     chars.next();
+    //     input = chars.as_str();
+    // }
+
+    let mut chars = input.chars();
+    while matches!(chars.next(), Some(_x @ ('.' | '0'..='9'))) {
+        input = chars.as_str();
+    }
+    (input, Some(Token::Number))
 }
 
-
-fn ident(mut input: &str) -> &str {
-    if matches!(input.chars().next(), Some(_x @ ('a'..='z'|'A'..='Z'))) {
-        let mut chars = input.chars();
-        while matches!(chars.next(), Some(_x @ ('a'..='z'|'A'..='Z'| '0'..='9'))) {
-            input  =chars.as_str();
-        }
+fn ident(mut input: &str) -> (&str, Option<Token>) {
+    if !matches!(input.chars().next(), Some(_x @ ('a'..='z' | 'A'..='Z'))) {
+        return (input, None);
     }
-    input
+    let mut chars = input.chars();
+    while matches!(chars.next(), Some(_x @ ('a'..='z' | 'A'..='Z' | '0'..='9'))) {
+        input = chars.as_str();
+    }
+    (input, Some(Token::Ident))
 }
 
 #[cfg(test)]
 mod test {
-    use std::io::Cursor;
-
     use super::*;
 
     #[test]
@@ -51,49 +86,63 @@ mod test {
         assert_eq!(result, "abc");
     }
 
-
     #[test]
     fn test_white_space_2() {
         let result = whitespace("abc");
         assert_eq!(result, "abc");
     }
 
-
     #[test]
     fn test_number() {
         let result = number("12.34");
-        assert_eq!(result, "");
+        assert_eq!(result.0, "");
     }
-
 
     #[test]
     fn test_number_2() {
         let result = number("12.34  ");
-        assert_eq!(result, "  ");
+        assert_eq!(result.0, "  ");
     }
-
 
     #[test]
     fn test_ident() {
         let result = ident("hogehoge  ");
-        assert_eq!(result, "  ");
+        assert_eq!(result.0, "  ");
     }
-
 
     #[test]
     fn test_ident_2() {
         let result = ident("hoge");
-        assert_eq!(result, "");
+        assert_eq!(result.0, "");
     }
 
+
+    #[test]
+    fn test_source() {
+        let result = source("123 world");
+        assert_eq!(result, vec![Token::Number, Token::Ident]);
+    }
+
+
+    #[test]
+    fn test_source_2() {
+        let result = source("Hello world");
+        assert_eq!(result, vec![Token::Ident, Token::Ident]);
+    }
+
+
+    #[test]
+    fn test_source_3() {
+        let result = source("      world");
+        assert_eq!(result, vec![Token::Ident]);
+    }
+
+    #[test]
+    fn test_source_empty() {
+        let result = source("");
+        assert_eq!(result, vec![]);
+    }
 }
 
-fn main() {
+fn main() {}
 
-}
-// fn main() {
-//     let mut a = "    vv ## ##";
-//     let result = whitespace(&mut a);
-//     dbg!(result, a);
-    
-// }
